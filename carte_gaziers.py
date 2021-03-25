f3e5ea999903f17755c4e6cb56ae13d48dad9c71
Kevin.Clement <kevin.clement@dnvgl.com>
2021-03-25 21:33:10 +0100

    Mise à jour Panda et Kévin

--- carte_gaziers.py
+++ carte_gaziers.py
@@ -1,14 +1,14 @@
 import folium
 import numpy as np
 
-gaziers = [{"nom": "Panda", "lat": 45.239055, "lon": 3.958689, "image": "https://dl2.macupdate.com/images/icons256/52971.png"},
+gaziers = [{"nom": "Panda", "lat": 45.5182122, "lon": -73.5727788, "image": "https://dl2.macupdate.com/images/icons256/52971.png"},
 {"nom": "Martin", "lat": 43.55, "lon": 6.93, "image": "https://pupuphooray.com/wp-content/uploads/2019/03/dog-icon.png"},
 {"nom": "Thibault", "lat": 47.216307, "lon": -1.558252, "image": "https://images.emojiterra.com/twitter/v12/512px/1f414.png"},
 {"nom": "Gaspard", "lat": 45.5182122, "lon": -73.5727788, "image": "https://www.pngrepo.com/png/232688/170/baboon.png"},
 {"nom": "Mathieu", "lat": 43.3, "lon": 5.4, "image": "https://cdn1.iconfinder.com/data/icons/animal-flat-2/128/animal_buffalo-u-zoo-512.png"},
 {"nom": "Baptiste", "lat": -33.818398, "lon": 150.972911, "image": "https://cdn2.iconfinder.com/data/icons/animals-92/28/animal_lineal_color-19-512.png"},
 {"nom": "Antoine", "lat": 45.246792, "lon": 4.260668, "image": "https://i.pinimg.com/originals/ce/76/b8/ce76b874769fa0c3761bc0a9bc064309.png"},
-{"nom": "Kévin", "lat": 48.86, "lon": 2.33, "image": "https://cdn.iconscout.com/icon/premium/png-256-thumb/fox-91-563338.png"}
+{"nom": "Kévin", "lat": 43.60750775047381, "lon": 3.8740151541513663, "image": "https://cdn.iconscout.com/icon/premium/png-256-thumb/fox-91-563338.png"}
 ]
 
 epicentre = [np.mean([gazier["lat"] for gazier in gaziers]),
